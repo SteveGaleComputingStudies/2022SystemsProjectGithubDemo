70d45727e96d14425f1d6e8456fde709cda10ad3
Steve Gale <steve.gale@computingstudies.net>
2022-03-11 11:48:50 +1100

    test ok

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 ## start doc here
 
-docunebtation
+documentation
--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,4 @@
 print("Hello 2022 Systems project Class")
 print("changes to push using git command line")
 print(" changes to commit from VScode")
+print(" new change")
